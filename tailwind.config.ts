911e90eef6d154ee617e7930a327c94c0f48c3b9
Grin Roman <grinromchik@gmail.com>
2025-02-18 21:19:17 +0000

    feature: fonts dynamic load with fallback to default for apple and other devices

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
 
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 export default {
   darkMode: ["class"],
   content: [
@@ -9,6 +11,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
